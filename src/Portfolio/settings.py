efca721d924f80b2f04ff2b75e2959efd1dd8605
D3stinn3 <paynedestinne@gmail.com>
2023-05-30 10:51:41 +0300

    uRL rESOLVE

--- src/Portfolio/settings.py
+++ src/Portfolio/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-BASE_DIR = [
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 STATIC_ROOT = BASE_DIR.parent / 'local-cdn' / 'static'
