e110afce6ada73fad01a8c5f35bee1fa69a586f7
D3stinn3 <paynedestinne@gmail.com>
2023-05-31 13:55:56 +0300

    Git for npm and Custom css conf

--- src/base/views.py
+++ src/base/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def home_page(request):
     context = {}
     
-    return render(request, 'base.html', context)
+    return render(request, 'home.html', context)
