7bcaabe0f5d8d30d04578916ce13d6530f2ba947
D3stinn3 <paynedestinne@gmail.com>
2023-05-31 16:54:08 +0300

    Media Settings

--- src/Portfolio/settings.py
+++ src/Portfolio/settings.py
@@ -122,6 +122,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR.parent / 'local-cdn' / 'static'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR.parent / "src" / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
