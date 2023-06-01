9d59eb72607cc1bf6d5568f91f7c20790da2398a
D3stinn3 <paynedestinne@gmail.com>
2023-06-01 17:50:21 +0300

    Urls

--- src/base/urls.py
+++ src/base/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('profile/', views.profile_page, name="profile")
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
