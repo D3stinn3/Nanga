84a4a291bf0dcb748d83d403cf327da84597c820
D3stinn3 <paynedestinne@gmail.com>
2023-06-02 13:00:50 +0300

    Models

--- src/base/models.py
+++ src/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    # AbstractUser class implements a fully featured User model with admin-compliant permissions
+    name = models.CharField(max_length=100, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True, blank=True)
+    party = models.BooleanField(default=True, null=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
