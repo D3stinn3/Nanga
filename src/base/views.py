0923fb6f2ef883f9c55ebdaade41b217684b7abe
D3stinn3 <paynedestinne@gmail.com>
2023-06-03 12:21:19 +0300

    Last Last

--- src/base/views.py
+++ src/base/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def login_page(request):
+    
+    if request.method == "POST":
+        username = authenticate(
+            email=request.POST['email'],
+            password=request.POST['password']
+            )
+        
+        if username is not None:
+            login(request, username)
+            return redirect('profile')
+        
     context = {}
     
     return render(request, 'login.html', context)
@@ -18,6 +30,7 @@ def signup_page(request):
     
     return render(request, 'signup.html', context)
 
+@login_required(login_url='/login')
 def account_page(request, pk):
     user = request.user
     context = {'user' : user} 
