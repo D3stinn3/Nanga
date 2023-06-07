2fcbe6137336ce89a8eb6124ac4b410ca4e5f4b7
D3stinn3 <paynedestinne@gmail.com>
2023-06-07 14:53:03 +0300

    Models UUID

--- src/base/models.py
+++ src/base/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+import uuid
 # Create your models here.
 class User(AbstractUser):
     # AbstractUser class implements a fully featured User model with admin-compliant permissions
@@ -8,6 +8,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True, blank=True)
     party = models.BooleanField(default=True, null=True)
-    
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
