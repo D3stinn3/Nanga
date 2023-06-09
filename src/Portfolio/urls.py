1f093c1ca40862ce5a49f8831d9eba775d2ffbdf
D3stinn3 <paynedestinne@gmail.com>
2023-06-09 17:26:16 +0300

    Url Change

--- src/Portfolio/urls.py
+++ src/Portfolio/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('base.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/base/urls.py
+++ src/base/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('account/', views.account_page, name="account")
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
